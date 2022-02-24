b59efae49d9f9c3cfc31a8dcca6ea63695611fe1
Justin Lepine <lepine.justin@yahoo.com>
2022-02-23 20:18:25 -0500

    video layout done with test video

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav/Nav.js";
+import Video from "./components/Video/Video.js";
 import "./App.scss";
 
 function App() {
   return (
     <>
       <Nav />
+      <Video />
     </>
   );
 }
---
+++ src/components/Video/Video.js
@@ -0,0 +1,12 @@
+import "./Video.scss";
+
+function Video() {
+  return (
+<>
+{/* <video><source width="420" height="345"  src="https://www.youtube.com/watch?v=OJnsdRqhR0M" type="video/mp4"/></video> */}
+<iframe title="test" className="video" src="https://player.vimeo.com/video/24229690?h=19a65fbefe" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen></iframe>
+    </>
+  )
+}
+
+export default Video
