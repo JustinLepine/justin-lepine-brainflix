44f2f49fd3b60a32c5eee0a0af1c72fb97fd1d4d
Justin Lepine <lepine.justin@yahoo.com>
2022-02-28 20:31:13 -0500

    added uploads page and linked through nav logo and button

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,20 +3,23 @@ import logo from "../../assets/logos/BrainFlix-logo.svg";
 import searchIcon from "../../assets/icons/search.svg";
 import uploadIcon from "../../assets/icons/upload.svg";
 import avatar from "../../assets/images/Mohan-muruge.jpg";
+import { Link } from "react-router-dom";
 
 function Nav() {
     return (
         <nav className="nav">
-            <img src={logo} className="nav__logo" alt="brainflix-logo" />            
+            <Link to="/">
+                <img src={logo} className="nav__logo" alt="brainflix-logo" />            
+            </Link>
             <form className="nav__form">
                 <img src={searchIcon} className="nav__search" alt="search-icon"/>
                 <input className="nav__input" placeholder="Search"></input>
             </form>
             <img src={avatar} className="nav__avatar nav__avatar--phone" alt="avatar"/>
-            <div className="nav__button">
+            <Link to="/uploads" className="nav__button">
                 <img src={uploadIcon} className="nav__icon" alt="upload-icon"/>
-                <a href="/" className="nav__upload">UPLOAD</a>
-            </div>
+                <div href="/" className="nav__upload">UPLOAD</div>
+            </Link>
             <img src={avatar} className="nav__avatar nav__avatar--tablet" alt="avatar"/>
         </nav>
     );
