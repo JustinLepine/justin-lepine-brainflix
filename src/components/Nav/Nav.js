25a5a10f9fe9f9f7e8052730fe0e43d2050c270e
Justin Lepine <lepine.justin@yahoo.com>
2022-02-28 21:54:45 -0500

    upload page styling complete in phone size

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 function Nav() {
     return (
         <nav className="nav">
+
             <Link to="/">
                 <img src={logo} className="nav__logo" alt="brainflix-logo" />            
             </Link>
@@ -15,12 +16,14 @@ function Nav() {
                 <img src={searchIcon} className="nav__search" alt="search-icon"/>
                 <input className="nav__input" placeholder="Search"></input>
             </form>
+
             <img src={avatar} className="nav__avatar nav__avatar--phone" alt="avatar"/>
             <Link to="/uploads" className="nav__button">
                 <img src={uploadIcon} className="nav__icon" alt="upload-icon"/>
                 <div href="/" className="nav__upload">UPLOAD</div>
             </Link>
             <img src={avatar} className="nav__avatar nav__avatar--tablet" alt="avatar"/>
+            
         </nav>
     );
 }
--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import Sample from "../../assets/images/Mohan-muruge.jpg";
+import publishIcon from "../../assets/icons/publish.svg";
+import "./Upload.scss";
 
 function Upload() {
   return (
-    <div>
-      
-    </div>
+    <section className="upload">
+      <h1 className="upload__title">Upload Video</h1>
+      <h5 className="upload__subtitle">VIDEO THUMBNAIL</h5>
+      <img className="upload__picture" src={Sample} alt="video sample" />
+      <h5 className="upload__subtitle">TITLE YOUR VIDEO</h5>
+      <textarea className='upload__video-title' id="uploadVideoTitle" placeholder="Add a title to your video"></textarea>
+      <h5 className="upload__subtitle">ADD A VIDEO DESCRIPTION</h5>
+      <textarea className='upload__video-desc' id="uploadVideoTitle" placeholder="Add a description to your video"></textarea>
+      <div className="upload__button">
+        <img src={publishIcon} className="upload__icon" alt="publish-icon" />
+        <a href="/" className="upload__publish">PUBLISH</a>
+      </div>
+      <div className="upload__cancel-button">
+        <a href="/" className="upload__cancel">CANCEL</a>
+      </div>
+    </section>
   )
 }
 
-export default Upload
+export default Upload;
