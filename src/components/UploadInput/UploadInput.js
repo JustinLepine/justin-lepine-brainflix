e033626c98a496fa843064ee34c763e1e555852c
Justin Lepine <lepine.justin@yahoo.com>
2022-03-09 18:59:43 -0500

    changed upload section to form

--- src/components/UploadInput/UploadInput.js
+++ src/components/UploadInput/UploadInput.js
@@ -3,12 +3,12 @@ import "./UploadInput.scss";
 
 function UploadInput() {
   return (
-    <section className="upload-input">
+    <form className="upload-input">
       <h5 className="upload-input__subtitle">TITLE YOUR VIDEO</h5>
       <textarea className='upload-input__video-title' id="uploadInputVideoTitle" placeholder="Add a title to your video"></textarea>
       <h5 className="upload-input__subtitle">ADD A VIDEO DESCRIPTION</h5>
       <textarea className='upload-input__video-desc' id="uploadInputVideoTitle" placeholder="Add a description to your video"></textarea>
-    </section>      
+    </form>      
   )
 }
 
